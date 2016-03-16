e1186d5f7c753512959640f7657a9c13c699207c
John the Scott <jmscott@setspace.com>
2016-03-16 08:03:01 -0500

    added argv instructions

--- hoq/machine.go
+++ hoq/machine.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 // bool_value is result of AND, OR and relational operations
@@ -11,6 +12,7 @@ type bool_value struct {
 
 	*flow
 }
+type bool_chan chan *bool_value
 
 func (bv *bool_value) String() string {
 	if bv == nil {
@@ -22,16 +24,22 @@ func (bv *bool_value) String() string {
 	return fmt.Sprintf("%t", bv.bool)
 }
 
-type bool_chan chan *bool_value
-
 type string_value struct {
 	string
 	is_null bool
 
 	*flow
 }
-
 type string_chan chan *string_value
+
+type exit_status_value struct {
+	uint8
+	is_null bool
+	called bool
+
+	*flow
+}
+type exit_status_value_chan chan *exit_status_value
 
 type argv_value struct {
 	argv    []string
@@ -39,7 +47,6 @@ type argv_value struct {
 
 	*flow
 }
-
 type argv_chan chan *argv_value
 
 //  a flow tracks the firing of rules over a single line of input text.
@@ -58,7 +65,7 @@ type flow struct {
 	//  tab separated fields split out from the line read from
 	//  standard input
 
-	fields	[]int
+	fields	[]string
 
 	//  count of go routines still flowing expressions
 	confluent_count int
@@ -198,9 +205,9 @@ func (flo *flow) const_string(s string) (out string_chan) {
 	return out
 }
 
-//  send a field from the input line
+//  send the i'th tab separated field of the input line
 
-func (flo *flow) dollar(field int) (out string_chan) {
+func (flo *flow) dollar(i int) (out string_chan) {
 
 	out = make(string_chan)
 
@@ -209,15 +216,258 @@ func (flo *flow) dollar(field int) (out string_chan) {
 
 		for flo = flo.get(); flo != nil; flo = flo.get() {
 			
-			if field < len(flo.fields)
+			sv := &string_value{
+				flow:	flo,
+			}
+			if i < len(flo.fields) {
+				sv.string = flo.fields[i]
+			} else {
+				sv.is_null = true
+			}
+			out <- sv
+		}
+	}()
 
-			out <- &string_value{
-				string:  s,
+	return out
+}
+//  a rule fires if and only if both the argv[] exists and
+//  the when clause is true.
+
+func (flo *flow) wait_fire(
+	in_argv argv_chan,
+	in_when bool_chan,
+) (
+	argv *argv_value,
+	when *bool_value,
+) {
+
+	//  wait for both an argv[] and resolution of the when clause
+	for argv == nil || when == nil {
+		select {
+		case argv = <-in_argv:
+			if argv == nil {
+				return nil, nil
+			}
+
+		case when = <-in_when:
+			if when == nil {
+				return nil, nil
+			}
+		}
+	}
+	return
+}
+
+//  empty non-null argv sends immediatly
+
+func (flo *flow) argv0() (out argv_chan) {
+
+	out = make(argv_chan)
+
+	go func() {
+		defer close(out)
+
+		var argv [0]string
+
+		for flo = flo.get(); flo != nil; flo = flo.get() {
+
+			out <- &argv_value{
 				is_null: false,
+				argv:    argv[:],
+				flow:    flo,
+			}
+		}
+	}()
+	return out
+}
+
+//  direct answer of argv with single string
+
+func (flo *flow) argv1(in string_chan) (out argv_chan) {
+
+	out = make(argv_chan)
+
+	go func() {
+		defer close(out)
+
+		var argv [1]string
+
+		for flo = flo.get(); flo != nil; flo = flo.get() {
+
+			sv := <-in
+			if sv == nil {
+				return
+			}
+			argv[0] = sv.string
+			out <- &argv_value{
+				is_null: sv.is_null,
+				argv:    argv[:],
+				flow:    flo,
+			}
+		}
+	}()
+	return out
+}
+
+//  read strings from multiple input channels and write assmbled argv[]
+//  any null value renders the whole argv[] null
+
+func (flo *flow) argv(in_args []string_chan) (out argv_chan) {
+
+	//  track a received string and position in argv[]
+	type arg_value struct {
+		*string_value
+		position uint8
+	}
+
+	out = make(argv_chan)
+	argc := uint8(len(in_args))
+
+	//  called func has arguments, so wait on multple string channels
+	//  before sending assembled argv[]
+
+	go func() {
+
+		defer close(out)
+
+		//  merge() many string channels onto a single channel of
+		//  argument values.
+
+		merge := func() (mout chan arg_value) {
+
+			var wg sync.WaitGroup
+			mout = make(chan arg_value)
+
+			io := func(sc string_chan, p uint8) {
+				for sv := range sc {
+					mout <- arg_value{
+						string_value: sv,
+						position:     p,
+					}
+				}
+				wg.Done()
+			}
+
+			wg.Add(len(in_args))
+			for i, sc := range in_args {
+				go io(sc, uint8(i))
+			}
+
+			//  Start a goroutine to close 'mout' channel
+			//  once all the output goroutines are done.
+
+			go func() {
+				wg.Wait()
+				close(mout)
+			}()
+			return
+		}()
+
+		for flo = flo.get(); flo != nil; flo = flo.get() {
+
+			av := make([]string, argc)
+			ac := uint8(0)
+			is_null := false
+
+			//  read until we have an argv[] for which all elements
+			//  are also non-null.  any null argv[] element makes
+			//  the whole argv[] null
+
+			for ac < argc {
+
+				a := <-merge
+
+				//  Note: compile generates error for
+				//        arg_value{}
+
+				if a == (arg_value{}) {
+					return
+				}
+
+				sv := a.string_value
+				pos := a.position
+
+				//  any null element forces entire argv[]
+				//  to be null
+
+				if a.is_null {
+					is_null = true
+				}
+
+				//  cheap sanity test tp insure we don't
+				//  see the same argument twice
+				//
+				//  Note:
+				//	technically this implies an empty
+				//	string is not allowed which is probably
+				//	unreasonable
+
+				if av[pos] != "" {
+					panic("argv[] element not \"\"")
+				}
+				av[pos] = sv.string
+				ac++
+			}
+
+			//  feed the hungry world our new, boundless argv[]
+			out <- &argv_value{
+				argv:    av,
+				is_null: is_null,
 				flow:    flo,
 			}
 		}
 	}()
 
+	return out
+}
+
+func (flo *flow) call(
+	cmd *command,
+	in_argv argv_chan,
+	in_when bool_chan,
+) (out exit_status_value_chan) {
+
+	out = make(exit_status_value_chan)
+
+	go func() {
+		defer close(out)
+
+		for flo = flo.get(); flo != nil; flo = flo.get() {
+
+			argv, when := flo.wait_fire(in_argv, in_when)
+			if argv == nil {
+				return
+			}
+
+			//  don't fire command if either argv is null or
+			//  when is false or null
+
+			exv := &exit_status_value{
+				flow: flo,
+			}
+
+			switch {
+
+			//  uint8 is null when either argv or when is null
+
+			case argv.is_null || when.is_null:
+				exv.is_null = true
+
+			//  when is true and argv exists, so fire command
+
+			case when.bool:
+				//  synchronous call to command{} process
+				//  always returns partially built xdr value
+
+				exv.called = true
+				//xv = cmd.call(argv.argv, osx_q)
+			}
+
+			//  the flow never resolves until all call()s
+			//  generate an xdr record.
+
+			out <- exv
+		}
+	}()
 	return out
 }
