88bad93f37ab7c291e0e9a8f5a41ee9cb5fc6786
John the Scott <jmscott@setspace.com>
2016-05-20 20:54:13 -0500

    reformat code

--- pgfabric/who-blocks-whom.sql
+++ pgfabric/who-blocks-whom.sql
@@ -16,7 +16,7 @@ SELECT
   FROM
   	pg_catalog.pg_locks blockedl
   	JOIN pg_stat_activity blockeda ON blockedl.pid = blockeda.pid
-  	JOIN pg_catalog.pg_locks blockingl ON(
+  	JOIN pg_catalog.pg_locks blockingl ON (
 		blockingl.transactionid = blockedl.transactionid
   		AND
 		blockedl.pid != blockingl.pid
