313bc7719292079a7632488c1670f8e2462dbdaa
John the Scott <jmscott@setspace.com>
2017-09-30 16:58:27 -0500

    added cache-hit-rate.sql

---
+++ pgfabric/pgsql-9.6/cache-hit-rate.sql
@@ -0,0 +1,11 @@
+SELECT
+	sum(heap_blks_read) AS "Heap Blocks Read",
+	sum(heap_blks_hit)  AS "Heap Blocks Hit",
+	to_char(
+	  sum(heap_blks_hit) / (sum(heap_blks_hit) +
+	  sum(heap_blks_read)) * 100,
+	  'FM99.0') || '%'
+		AS "Cache Hit Ratio"
+  FROM
+  	pg_statio_user_tables
+;
