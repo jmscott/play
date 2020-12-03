0d5b35d14111516c274fab9f72dfd5912178d087
jmscott <jmscott@setspace.com>
2020-12-03 17:49:13 -0600

    check point

--- bit-entropy/shift-xor-pop-count-v1.c
+++ bit-entropy/shift-xor-pop-count-v1.c
@@ -1,9 +1,12 @@
 /*
  *  Synopsis:
- *	Count bits in blob XOR'ed with itself shifted one bit to the left.
+ *	Count bits in blob XOR'ed with itself shifted one bit to the left, V1.
  *  Usage:
- *	shift-xor-pop-count-v1 <bytes-count> <mask-bits>
+ *	shift-xor-pop-count-v1 <blob-size> <ignore-bits>
  */
+
+#define COMMON_NEED_SIZE64
+#define COMMON_NEED_READ
 
 #include "./common.c"
 
@@ -12,9 +15,34 @@ char	*prog = "shift-xor-pop-count-v1";
 int
 main(int argc, char **argv) {
 
+	uint64_t blob_size, ignore_bits;
+
 	(void)argv;
 	if (argc != 3)
 		die("wrong argument count: expected 2 args");
+
+	blob_size = parse_size64("blob-size", argv[1]);
+
+	ignore_bits = parse_size64("ignore-bits", argv[2]);
+	if (ignore_bits == 0)
+		die("ignore bits can not == 0");
+	if (ignore_bits >= blob_size)
+		die("ignore bits can not >= blob size");
+
+	void *buf = malloc(blob_size);
+	if (buf == NULL)
+		die2("malloc(blob) failed", argv[1]);
+
+	//  Slurp the entire blob into ram memory.
+	size_t nr;
+	void *p = buf;
+	while ((nr = _read(p, PIPE_MAX)) > 0) {
+		p += nr;
+		if ((uint64_t)(p - buf) > blob_size)
+			die("blob too big");
+	}
+	if ((uint64_t)(p - buf) != blob_size)
+		die("blob too small");
 
 	_exit(EXIT_OK);
 }
