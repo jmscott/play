9ef4da9c9bf500b7e2a23893c727539477c87208
jmscott <jmscott@setspace.com>
2020-12-03 17:48:03 -0600

    added functions parse_(size|uint)64

--- bit-entropy/common.c
+++ bit-entropy/common.c
@@ -8,13 +8,24 @@
 #include <sys/errno.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <limits.h>
 
-extern char *prog;			//  defined in program file.
+extern char *prog;			//  defined in main() file.
 
 #define EXIT_OK		0
 #define EXIT_FAIL	2
 #ifndef PIPE_MAX
 #define PIPE_MAX	4096
+#endif
+
+#ifdef COMMON_NEED_SIZE64
+#define COMMON_NEED_PARSE_UINT64
+#define COMMON_NEED_DIE2
+#endif
+
+#ifdef COMMON_NEED_PARSE_UINT64
+#define COMMON_NEED_DIE2
 #endif
 
 #ifdef COMMON_NEED_DIE2
@@ -127,4 +138,49 @@ again:
         if (nbytes > 0)
                 goto again;
 }
+#endif
+
+#ifdef COMMON_NEED_PARSE_UINT64
+
+/*
+ *  Parse an unsigned 64 bit integer or croak.
+ */
+static uint64_t
+parse_uint64(char *what, char *src)
+{
+	size_t len;
+
+	len = strlen(src);
+	if (len == 0)
+		die2(what, "zero length");
+	if (len > 20)
+		die2(what, "length greater than 20 chars");
+	
+	//  only digits 0-9 allowed
+	for (size_t i = 0;  i < len;  i++)
+		if (src[i] < '0' || src[i] > '9')
+			die2(what, "non-digit not valid");
+
+	errno = 0;
+	unsigned long long ui64 = strtoull(src, (char **)0, 10);
+	if (ui64 == ULLONG_MAX && errno != 0)
+		die2(what, strerror(errno));
+
+	return (uint64_t)ui64;
+}
+#endif
+
+#ifdef COMMON_NEED_SIZE64
+/*
+ *  Parse unsigned 64 bit <= max of a signed 64 bit int.
+ */
+static uint64_t
+parse_size64(char *what, char *src)
+{
+	uint64_t size64 = parse_uint64(what, src);
+	if (size64 > LLONG_MAX)
+		die2(what, "size > max int64");
+	return size64;
+}
+
 #endif
