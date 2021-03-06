ab8c4966832c874f7407f5953edcbbca98a68632
John the Scott <jmscott@setspace.com>
2021-03-06 16:15:51 -0600

    added min/max sizes to daily zip

--- secedgar/report.sql
+++ secedgar/report.sql
@@ -2,6 +2,8 @@
  *  Synopsis:
  *	Summarize json for daily edgar pull from sec database.
  *  Note:
+ *	Query slowed dramatically when adding min/max zip sizes.  why?
+ *
  *	Do a daily summary of all jobs.
  *
  *	Investigate creating parital index on
@@ -11,6 +13,8 @@
 \set ON_ERROR_STOP 1
 \timing 1
 \x on
+
+SET search_path TO secedgar,public;
 WITH put_daily AS (
   SELECT
   	doc
@@ -26,7 +30,7 @@ WITH put_daily AS (
 			."command"
 		? (@ == "edgar-put-daily")
 	')
-), recent_zip AS(
+), recent_zip AS (
   SELECT
   	(doc->>'zip-blob')::udig AS blob,
   	substring(doc->>'zip-path' FROM '[^/]+$') AS zip_name
@@ -36,24 +40,38 @@ WITH put_daily AS (
     	2 DESC
     LIMIT
     	1
-)
-SELECT
+), zip_sizes AS (
+  SELECT
+  	min(bc.byte_count) AS "min_size",
+  	max(bc.byte_count) AS "max_size"
+    FROM
+    	daily_zip d
+	  JOIN setcore.byte_count bc ON (
+	  	bc.blob = bc.blob
+	  )
+	  
+) SELECT
 	count(d.doc) || ' Invocations' AS "edgar-put-daily",
 	min((d.doc->'now')::text::timestamp) AS "Earliest Job Time",
 	max((d.doc->'now')::text::timestamp) AS "Recent Job Time",
 	r.zip_name AS "Recent Zip",
 	r.blob AS "Recent Zip Blob",
-	pg_size_pretty(bc.byte_count) AS "Zip Size"
+	pg_size_pretty(bc.byte_count) AS "Recent Zip Size",
+	pg_size_pretty(sz.min_size) AS "Min Zip Size",
+	pg_size_pretty(sz.max_size) AS "Max Zip Size"
   FROM
  	put_daily d,
 	recent_zip r
 	  JOIN setcore.byte_count bc ON (
 	  	bc.blob = r.blob
-	  )
+	  ),
+	zip_sizes sz
   GROUP BY
   	r.blob,
 	r.zip_name,
-	bc.byte_count
+	bc.byte_count,
+	sz.min_size,
+	sz.max_size
   ORDER BY
   	random()
 ;
