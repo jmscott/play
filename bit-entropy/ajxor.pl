4d05e7b4d43e02f7143c4d11e302375b1c693df5
jmscott <jmscott@setspace.com>
2021-04-17 18:56:50 -0500

    first cut that makes only single bit vector

---
+++ bit-entropy/ajxor.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+#
+#  Synopsis:
+#	Derive a population vector of adjacent xors of a bit sequence.
+#  Usage:
+#	echo '01010101  11110000  00100110  11110000' | ajxor.pl
+#  See:
+#	https://github.com/jmscott/play/tree/master/bit-entropy
+#
+
+my $in;
+while (<STDIN>) {
+	s/[^01]*//g;
+	$in .= $_;
+}
+#print "in	$in\n";
+
+sub XOR
+{
+	my $p = shift;
+	my $q = shift;
+
+	return ($p eq $q) ? '0' : '1';
+}
+
+print 'ajxor	';
+my $in_length = length($in) - 1;
+for (my $i = 0;  $i < $in_length;  $i++) {
+	print '  ' if ($i > 0 && ($i % 8 == 0));
+	print XOR(substr($in, $i, 1), substr($in, $i + 1, 1));
+}
+print "\n";
