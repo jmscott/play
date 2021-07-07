3c91b2b9a8d4f4db5867a019148ae79e3617c1df
John the Scott <jmscott@setspace.com>
2021-07-06 21:40:48 -0500

    added schema.sql

---
+++ uspto/schema.sql
@@ -0,0 +1,15 @@
+/*
+ *  Synopsis:
+ *	SQL schema for United States Patent and Trademark bulk data.
+ */
+
+\set ON_ERROR_STOP 1
+
+BEGIN;
+
+DROP SCHEMA IF EXISTS uspto CASCADE;
+CREATE SCHEMA uspto;
+
+CREATE TABLE scan_xml_declaration
+
+END;
