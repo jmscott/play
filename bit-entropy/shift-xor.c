b731972be50334568131181a784b970fe2647471
John the Scott <jmscott@setspace.com>
2021-09-29 13:23:37 -0500

    unstable checkpoint

--- bit-entropy/shift-xor.c
+++ bit-entropy/shift-xor.c
@@ -1,10 +1,16 @@
 /*
  *  Synopsis:
- *	Count bits in blob XOR'ed with itself shifted one bit to the left, V1.
+ *	XOR stdin with itself shifted left one bit and write to stdout.
+ *  Description:
+ *	Write a new blob which is the XOR of the standard input shifted
+ *	one bit to the left.  The output is two lines followed by the
+ *	raw bytes
+ *
+ *		blob-size
+ *		ignore-tail-bits
+ *		bytes of shifted xor bytes
  *  Usage:
- *	shift-xor-pop-count-v1 <blob-size> <ignore-bits>
- *  Note:
- *	Incorrect algo for blob-size == 1
+ *	shift-xor <in-blob-size> <ignore-tail-bits>
  */
 
 #define COMMON_NEED_SIZE64
@@ -18,47 +24,51 @@ char	*prog = "shift-xor";
 int
 main(int argc, char **argv) {
 
-	uint64_t blob_size, ignore_bits;
+	uint64_t in_size, ignore_tail_bits;
 
 	if (argc != 3)
 		die("wrong argument count: expected 2 args");
 
-	blob_size = parse_size64("blob-size", argv[1]);
+	in_size = parse_size64("in-blob-size", argv[1]);
+	if (in_size == 0) {
+		write(1, "0\n0\n", 4);
+		_exit(0);
+	}
 
-	ignore_bits = parse_size64("ignore-bits", argv[2]);
-	if (ignore_bits == 0)
-		die("ignore bits can not == 0");
-	if (ignore_bits >= blob_size)
-		die("ignore bits can not >= blob size");
+	ignore_tail_bits = parse_size64("ignore-tail-bits", argv[2]);
+	if (ignore_tail_bits >= 8)
+		die("ignore bits can not >= 8");
 
-	void *blob = malloc(blob_size * 2);
-	if (blob == NULL)
-		die2("malloc(blob+xor) failed", argv[1]);
-	void *xor = blob + blob_size;
+	unsigned char *in = malloc(in_size * 2);
+	if (in == NULL)
+		die2("malloc(stdin+stdout) failed", argv[1]);
 
 	//  Slurp the entire blob into ram memory.
 	size_t nr;
-	void *p = blob;
+	unsigned char *p = in;
 	while ((nr = _read(p, PIPE_MAX)) > 0) {
 		p += nr;
-		if ((uint64_t)(p - blob) > blob_size)
+		if ((uint64_t)(p - in) > in_size)
 			die("blob too big");
 	}
-	if ((uint64_t)(p - blob) != blob_size)
+	if ((uint64_t)(p - in) != in_size)
 		die("blob too small");
 
-	unsigned char *b = (unsigned char *)blob;
-	unsigned char *b_limit = b + blob_size - 1;
-	unsigned char *x = (unsigned char *)xor;
+	p = in;
+	unsigned char *p_limit = in + in_size - 1;
+	unsigned char *x = in + in_size;
 
+	/*
+	 *  xor the in blob with the shifted version of itself.
+	 */
 	unsigned char c;
-	while (b < b_limit) {
-		c = *b++;
-		*x++ = (c ^ (c << 1)) | ((c & 0x1) ^ ((*b >> 7) & 0x1));
+	while (p < p_limit) {
+		c = *p++;
+		*x++ = (c ^ (c << 1)) | ((c & 0x1) ^ ((*p >> 7) &0x1));
 	}
-	c = *b;
-	*x = (c ^ (c << 1)) | ((c & 0x1) ^ ((*b >> 7) & 0x1));
-	_write(xor, blob_size);
+	c = *p;
+	// *x = (c ^ (c << 1)) | ((c & 0x1) ^ ((*b >> 7) & 0x1));
+	_write(in + in_size, in_size);
 
 	_exit(EXIT_OK);
 }
