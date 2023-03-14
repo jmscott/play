f49b15025b0f7792b8b314029b3d3191c3857f7b
John the Scott <jmscott@setspace.com>
2023-03-14 14:36:15 -0500

    first stable json output

--- postfix3/log2json.go
+++ postfix3/log2json.go
@@ -2,37 +2,76 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha1"
+	"crypto/sha512"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 )
 
-func
-die(what string, err error) {
+type Run struct {
+        LineCount		int64	`json:"line_count"`
+        ByteCount		int64	`json:"byte_count"`
+	xx512x1			[20]byte
+	InputDigest		string	`json:"input_digest"`
+	InputDigestAlgorithm	string	`json:"input_digest_algorithm"`
+}
+
+func die(what string, err error) {
 	fmt.Fprintf(os.Stderr, "ERROR: %s failed: %s", what, err) 
 	os.Exit(1)
 }
 
-func
-leave(exit_status int) {
+func leave(exit_status int) {
 	os.Exit(exit_status)
 }
 
-func
-main() {
+//
+//  To calulate "x2x512x1" hash at command line, do the following:
+//
+//	openssl dgst -binary -sha512			|
+//		openssl dgst -binary -sha512		|
+//		openssl dgst -sha1 -r
+//
+//  Free dinner for first who finds collision, valid until first quantum
+//  computer breaks crypto in the wild.
+//
 
-	line_count := uint64(0)
+func xx512x1(inner_512 []byte) [20]byte {
+	outer_512 := sha512.Sum512(inner_512)
+	return sha1.Sum(outer_512[:])
+}
+
+func main() {
+
+	var run Run
+
+	run.InputDigestAlgorithm = "xx512x1"
+
+	h512 := sha512.New()
 	in := bufio.NewReader(os.Stdin)
 	for {
-		_, err := in.ReadString('\n')
+		line, err := in.ReadBytes('\n')
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
-			die("bufio.ReadString(Stdin)", err)
+			die("bufio.ReadBytes(Stdin)", err)
 		}
-		line_count++
+		run.ByteCount += int64(len(line))
+		run.LineCount++
+		h512.Write(line)
 	}
-	fmt.Printf("line_count: %d\n", line_count)
+	run.xx512x1 = xx512x1(h512.Sum(nil))
+	run.InputDigest = fmt.Sprintf("%x", run.xx512x1)
 
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "	")
+	err := enc.Encode(&run)
+	if err != nil {
+		die("enc.Encode(json)", err) 
+	}
+
+	os.Exit(0)
 }
