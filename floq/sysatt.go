402024bcc7c81a0cfcd145e810547702279e585a
John the Scott <jmscott@setspace.com>
2025-08-23 02:16:04 -0500

    added DAG test for sysatt vars

---
+++ floq/sysatt.go
@@ -0,0 +1,22 @@
+package main
+
+type sysatt struct {
+
+	name		string
+	command_ref	*command
+}
+
+func (sa *sysatt) is_uint64() bool {
+
+	if sa.command_ref != nil {
+		return sa.command_ref.is_sysatt_uint64(sa.name)
+	}
+	return false
+}
+
+func (sa *sysatt) full_name() string {
+	if sa.command_ref != nil {
+		return sa.command_ref.name + "$" + sa.name
+	}
+	return ""
+}
