d8247c0614ce9d369077a8e88d9f164340536d15
John the Scott <jmscott@setspace.com>
2026-04-17 13:56:34 -0500

    improved SOGQUIT trace format

--- floq/floq.go
+++ floq/floq.go
@@ -75,7 +75,7 @@ func main() {
 		if s == syscall.SIGQUIT {
 			buf := make([]byte, 1<<20)
 			len := runtime.Stack(buf, true)
-			fmt.Printf("=== Stack Trace ===\n%s\n", buf[:len])
+			fmt.Printf("\n=== Stack Trace ===\n%s\n", buf[:len])
 		}
 		fmt.Fprintf(os.Stderr, "\nfloq: caught signal: %s\n", s)
 		os.Exit(0)
